4ded128cc2a8f0cc50c85b481ad0887c9b2ee6cf
Antonio Raimundo <antoniorrm@sysautomacao.com.br>
2021-07-13 17:12:38 -0300

    feat: theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, ThemeProvider } from '@material-ui/core';
 import './App.css';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+			<ThemeProvider theme={theme}>
+				{/* <GlobalStyles /> */}
+				{/* {routing} */}
+				<Button color='primary'>Primary</Button>
+				<Button color='secondary'>Secondary</Button>
+			</ThemeProvider>
+		)
 }
 
 export default App;
---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+import { createTheme, colors, ThemeOptions } from '@material-ui/core'
+
+const theme = createTheme({
+	palette: {
+		background: {
+			dark: '#F4F6F8',
+			default: colors.common.white,
+			paper: colors.common.white,
+		},
+		primary: {
+			main: colors.indigo[500],
+		},
+		secondary: {
+			main: colors.indigo[500],
+		},
+		text: {
+			primary: colors.blueGrey[900],
+			secondary: colors.blueGrey[600],
+		},
+	},
+} as ThemeOptions)
+
+export default theme
