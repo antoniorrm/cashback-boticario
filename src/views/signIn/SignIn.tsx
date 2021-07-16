a47bba827057b0ec42fe9db608e4ac12723e383c
Antonio <araimundorm@gmail.com>
2021-07-15 22:53:08 -0300

    refactor: logo and titles

--- src/views/signIn/SignIn.tsx
+++ src/views/signIn/SignIn.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import TextField from '@material-ui/core/TextField'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import { Grid, Link } from '@material-ui/core'
 import { NavLink } from 'react-router-dom'
+
+import LogoBoticatio from '../../assets/img/logo-boticario-primary.svg'
 
 const useStyles = makeStyles(theme => ({
 	paper: {
@@ -17,16 +17,16 @@ const useStyles = makeStyles(theme => ({
 		flexDirection: 'column',
 		alignItems: 'center',
 	},
-	avatar: {
-		margin: theme.spacing(1),
-		backgroundColor: theme.palette.secondary.main,
-	},
 	form: {
 		width: '100%', // Fix IE 11 issue.
 		marginTop: theme.spacing(1),
 	},
 	submit: {
 		margin: theme.spacing(3, 0, 2),
+	},
+	img: {
+		width: '80%',
+		margin: 4,
 	},
 }))
 
@@ -37,9 +37,7 @@ export default function SignIn() {
 		<Container component='main' maxWidth='xs'>
 			<CssBaseline />
 			<div className={classes.paper}>
-				<Avatar className={classes.avatar}>
-					<LockOutlinedIcon />
-				</Avatar>
+				<img className={classes.img} src={LogoBoticatio} alt='Logo empresa' />
 				<Typography component='h1' variant='h5'>
 					Acessar Sistema
 				</Typography>
@@ -80,7 +78,7 @@ export default function SignIn() {
 						color='primary'
 						className={classes.submit}
 					>
-						Sign In
+						Acessar
 					</Button>
 				</form>
 			</div>
--- src/views/signUp/SignUp.tsx
+++ src/views/signUp/SignUp.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import TextField from '@material-ui/core/TextField'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import { Grid } from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
+
+import LogoBoticatio from '../../assets/img/logo-boticario-primary.svg'
 
 const useStyles = makeStyles(theme => ({
 	paper: {
@@ -17,16 +17,16 @@ const useStyles = makeStyles(theme => ({
 		flexDirection: 'column',
 		alignItems: 'center',
 	},
-	avatar: {
-		margin: theme.spacing(1),
-		backgroundColor: theme.palette.secondary.main,
-	},
 	form: {
 		width: '100%', // Fix IE 11 issue.
 		marginTop: theme.spacing(1),
 	},
 	submit: {
 		margin: theme.spacing(3, 0, 2),
+	},
+    img: {
+		width: '80%',
+		margin: 4,
 	},
 }))
 
@@ -38,11 +38,9 @@ export default function SignUp() {
 		<Container component='main' maxWidth='xs'>
 			<CssBaseline />
 			<div className={classes.paper}>
-				<Avatar className={classes.avatar}>
-					<LockOutlinedIcon />
-				</Avatar>
+				<img className={classes.img} src={LogoBoticatio} alt='Logo empresa' />
 				<Typography component='h1' variant='h5'>
-					Sign up
+					Criar conta
 				</Typography>
 
 				<form className={classes.form} noValidate>
