43dd4bea55e37e33f3cdf5ebea7bcf603001039c
Antonio <araimundorm@gmail.com>
2021-07-16 22:19:38 -0300

    feat: cashbackCard component

---
+++ src/components/CashbackCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+	root: {
+		minWidth: 275,
+	},
+})
+
+export default function CashbackCard() {
+	const classes = useStyles()
+
+	return (
+		<Card className={classes.root}>
+			<CardContent>
+				<Typography variant='h5' component='h2'>
+					Cashback acumulado
+				</Typography>
+				<Typography variant='body2' component='p'>
+					R$ 100,00
+				</Typography>
+			</CardContent>
+		</Card>
+	)
+}
