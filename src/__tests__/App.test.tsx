6b08df9c0b646f6cb4caa152b62338e985c53e76
Antonio <araimundorm@gmail.com>
2021-07-19 01:06:26 -0300

    refactor: test to it

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react'
 import App from '../App'
 
 describe('App component', () => {
-	test('renders App component correctly', () => {
+	it('renders App component correctly', () => {
 		const { container } = render(<App />)
 		expect(container).toMatchSnapshot()
 	})
