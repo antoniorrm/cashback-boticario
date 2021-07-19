7f95f6d2f4ff80bd75fade4ffaf7db7937a08cf6
Antonio <araimundorm@gmail.com>
2021-07-19 13:29:39 -0300

    test: type ref

--- src/__tests__/pages/Home/components/PurchaseFormModal/index.test.tsx
+++ src/__tests__/pages/Home/components/PurchaseFormModal/index.test.tsx
@@ -3,10 +3,10 @@ import { render } from '@testing-library/react'
 import { SnackbarProvider } from 'notistack'
 import React from 'react'
 import { Provider } from 'react-redux'
-import PurchaseFormModal from '../../../../../pages/Home/components/PurchaseFormModal'
+import PurchaseFormModal, { ModalHandler } from '../../../../../pages/Home/components/PurchaseFormModal'
 import { store } from '../../../../../store'
 
-const ref = React.createRef()
+const ref = React.createRef<ModalHandler>()
 
 describe('PurchaseFormModal component', () => {
 	it('renders PurchaseFormModal component correctly', () => {
